472b199515fbeb04554897482bd38daae48a38cd
Nacho <ignacio.nahuel.ramos@gmail.com>
2022-12-11 13:20:05 -0300

    created correct logout test

--- authorization/tests.py
+++ authorization/tests.py
@@ -40,3 +40,24 @@ class UserTestCase(TestCase):
         self.assertIn('access', result)
         self.assertIn('refresh', result)
 
+    def test_user_logout(self):
+        client = APIClient()
+        response_login = client.post(
+            '/login/', {
+                "username": "testing_login@testing.com",
+                "password": "testing123"
+            },
+            format='json'
+        )
+        result = json.loads(response_login.content)
+        
+        client.credentials(HTTP_AUTHORIZATION='Bearer ' + result['access'])
+
+        response_logout = client.post(
+            '/logout/', {
+                "refresh_token": result['refresh'],
+            },
+            format='json'
+        )
+        self.assertEqual(response_logout.status_code, status.HTTP_200_OK)
+        self.assertEqual(response_logout.data['status'], 'OK, goodbye')
