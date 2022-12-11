fb78c583249dcefcea6040d102295bd373b1c970
Nacho <ignacio.nahuel.ramos@gmail.com>
2022-12-11 13:48:27 -0300

    create user with already in use email

--- authorization/tests/test_registration.py
+++ authorization/tests/test_registration.py
@@ -72,3 +72,23 @@ class UserRegistrationTestCase(TestCase):
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertEqual(response.data['errors']['password'], ['This field is required.'])
 
+    def test_username_already_exists_registration(self):
+        client = APIClient()
+        response = client.post(
+            '/register/', {
+                "username": "testing@testing.com",
+                "password": "testing123"
+            },
+            format='json'
+        )
+
+        response = client.post(
+            '/register/', {
+                "username": "testing@testing.com",
+                "password": "testing123"
+            },
+            format='json'
+        )
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(response.data['errors']['non_field_errors'], ['User with this email already exists'])
+    
